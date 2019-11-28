358946c4aafcab973985af20d7cad44d95af6a16
Soronkkk <soronk99@gmail.com>
2019-11-28 15:04:49 +0300

    cs-6-Добавить боковую панель, форму отправки сообщения и доработать сущность сообщений 1) Форму для отправки сообщения 2) Скроллинг для блоков на странице

--- src/main/java/com/project/pet/nuketyvkar/services/impl/MessageServiceImpl.java
+++ src/main/java/com/project/pet/nuketyvkar/services/impl/MessageServiceImpl.java
@@ -2,21 +2,31 @@ package com.project.pet.nuketyvkar.services.impl;
 
 import com.project.pet.nuketyvkar.model.Message;
 import com.project.pet.nuketyvkar.repositories.MessageRepository;
+import com.project.pet.nuketyvkar.repositories.UserRepository;
 import com.project.pet.nuketyvkar.services.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class MessageServiceImpl implements MessageService {
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     private MessageRepository messageRepository;
 
     @Transactional
     public Message save(Message message) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        message.setAuthor(userRepository.findUserByUsername(authentication.getName()).get());
+        message.setCreatedDateTime(LocalDateTime.now());
         return messageRepository.save(message);
     }
 
